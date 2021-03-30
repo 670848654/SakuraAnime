b53d35d1f99a37066e2137965fab6eafd4fb1089
670848654 <670848654@qq.com>
2021-03-30 15:43:05 +0800

    update

--- app/src/main/java/my/project/sakuraproject/api/Api.java
+++ app/src/main/java/my/project/sakuraproject/api/Api.java
@@ -11,7 +11,7 @@ public class Api {
     //检测更新
     public final static String CHECK_UPDATE = "https://api.github.com/repos/670848654/SakuraAnime/releases/latest";
     //樱花动漫解析Api
-    public final static String PARSE_API = "http://tup.yhdm.tv/?vid=%s";
+    public final static String PARSE_API = "http://tup.yhdm.io/?vid=%s";
     //imomoe视频解析Api
     public final static String IMOMOE_PARSE_API = "https://saas.jialingmm.net/code.php?type=%s&vid=%s&userlink=%s";
 }

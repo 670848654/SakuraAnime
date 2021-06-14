85a7e0d8ad2968b17c73aaae60aa2430301fa911
670848654 <670848654@qq.com>
2021-06-14 15:08:22 +0800

    add

---
+++ app/src/main/java/my/project/sakuraproject/custom/MyButton.java
@@ -0,0 +1,28 @@
+package my.project.sakuraproject.custom;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MyButton extends MaterialButton {
+
+    public MyButton(@NonNull Context context) { super(context); }
+
+    public MyButton(Context context, @Nullable AttributeSet attrs) { super(context, attrs); }
+
+    public MyButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) { super(context, attrs, defStyleAttr); }
+
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) { super.onFocusChanged(focused, direction, previouslyFocusedRect); }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) { if(hasWindowFocus) super.onWindowFocusChanged(hasWindowFocus); }
+
+    @Override
+    public boolean isFocused() { return true; }
+}

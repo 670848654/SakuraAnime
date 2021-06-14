e91bbbfad52ffe3bfcd8c129e6833453e1c3992f
670848654 <670848654@qq.com>
2021-06-14 17:45:09 +0800

    update

--- app/src/main/java/my/project/sakuraproject/main/player/ImomoePlayerActivity.java
+++ app/src/main/java/my/project/sakuraproject/main/player/ImomoePlayerActivity.java
@@ -191,6 +191,12 @@ public class ImomoePlayerActivity extends BaseActivity implements JZPlayer.Compl
                 drawerLayout.closeDrawer(GravityCompat.START);
             else drawerLayout.openDrawer(GravityCompat.START);
         });
+        player.openDrama.setOnClickListener(view -> {
+            if (!Utils.isFastClick()) return;
+            if (drawerLayout.isDrawerOpen(GravityCompat.END))
+                drawerLayout.closeDrawer(GravityCompat.END);
+            else drawerLayout.openDrawer(GravityCompat.END);
+        });
         player.setListener(this, this, this);
         player.backButton.setOnClickListener(v -> finish());
         // 加载视频失败，嗅探视频
